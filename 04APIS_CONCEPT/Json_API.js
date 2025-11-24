36ece89cc25d078250894501b444bdba638af0f3
Kulwinder kour <kkour8585@gmail.com>
2025-11-24 16:08:49 +0530

    APIS  files

---
+++ 04APIS_CONCEPT/Json_API.js
@@ -0,0 +1,27 @@
+/*
+
+this is the method of the to write the json (just objects only) BELOW
+{
+    }
+
+*/
+
+{
+    name: "bmw"
+    price :"1.2cr"
+    company: "bmw motors"
+}
+
+
+
+//
+//      IN FETCH METHOD PROVIDE THE URL AND IT RETURNS API OBJECTS      
+
+//       SOME API ARE IN THE ARRYAS FORMAT ALSO IN *ARRAYS FORM
+
+
+[
+    {},
+    {},
+    {}
+]
